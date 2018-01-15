353148cf4baeed24c4673648a89304565c6007d1
chengwei <chengwei@bilibili.com>
2018-01-15 20:52:56 +0800

    朴素贝叶斯

--- Ch03/trees.py
+++ Ch03/trees.py
@@ -6,6 +6,11 @@ Decision Tree Source Code for Machine Learning in Action Ch. 3
 '''
 from math import log
 import operator
+from matplotlib.font_manager import FontProperties
+import matplotlib.pyplot as plt
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 """
 函数说明:创建测试数据集
@@ -242,27 +247,189 @@ def grabTree(filename):
     import pickle
     fr = open(filename)
     return pickle.load(fr)
+
+"""
+函数说明:获取决策树叶子结点的数目
+
+Parameters:
+    myTree - 决策树
+Returns:
+    numLeafs - 决策树的叶子结点的数目
+Author:
+    Jack Cui
+Blog:
+    http://blog.csdn.net/c406495762
+Modify:
+    2017-07-24
+"""
+def getNumLeafs(myTree):
+    numLeafs = 0                                                #初始化叶子
+    firstStr = next(iter(myTree))                                #python3中myTree.keys()返回的是dict_keys,不在是list,所以不能使用myTree.keys()[0]的方法获取结点属性，可以使用list(myTree.keys())[0]
+    secondDict = myTree[firstStr]                                #获取下一组字典
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':                #测试该结点是否为字典，如果不是字典，代表此结点为叶子结点
+            numLeafs += getNumLeafs(secondDict[key])
+        else:   numLeafs +=1
+    return numLeafs
+
+"""
+函数说明:获取决策树的层数
+
+Parameters:
+    myTree - 决策树
+Returns:
+    maxDepth - 决策树的层数
+Author:
+    Jack Cui
+Blog:
+    http://blog.csdn.net/c406495762
+Modify:
+    2017-07-24
+"""
+def getTreeDepth(myTree):
+    maxDepth = 0                                                #初始化决策树深度
+    firstStr = next(iter(myTree))                                #python3中myTree.keys()返回的是dict_keys,不在是list,所以不能使用myTree.keys()[0]的方法获取结点属性，可以使用list(myTree.keys())[0]
+    secondDict = myTree[firstStr]                                #获取下一个字典
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':                #测试该结点是否为字典，如果不是字典，代表此结点为叶子结点
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:   thisDepth = 1
+        if thisDepth > maxDepth: maxDepth = thisDepth            #更新层数
+    return maxDepth
+
+"""
+函数说明:绘制结点
+
+Parameters:
+    nodeTxt - 结点名
+    centerPt - 文本位置
+    parentPt - 标注的箭头位置
+    nodeType - 结点格式
+Returns:
+    无
+Author:
+    Jack Cui
+Blog:
+    http://blog.csdn.net/c406495762
+Modify:
+    2017-07-24
+"""
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    import matplotlib as mpl  
+    mpl.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签  
+    mpl.rcParams['axes.unicode_minus']=False #用来正常显示负号  
+    arrow_args = dict(arrowstyle="<-")                                            #定义箭头格式
+    font = FontProperties(fname=r"'C:/Windows/Fonts/simkai.ttf", size=14)        #设置中文字体
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt,  xycoords='axes fraction',    #绘制结点
+        xytext=centerPt, textcoords='axes fraction',
+        va="center", ha="center", bbox=nodeType, arrowprops=arrow_args)
+
+"""
+函数说明:标注有向边属性值
+
+Parameters:
+    cntrPt、parentPt - 用于计算标注位置
+    txtString - 标注的内容
+Returns:
+    无
+Author:
+    Jack Cui
+Blog:
+    http://blog.csdn.net/c406495762
+Modify:
+    2017-07-24
+"""
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0]-cntrPt[0])/2.0 + cntrPt[0]                                            #计算标注位置                   
+    yMid = (parentPt[1]-cntrPt[1])/2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString, va="center", ha="center", rotation=30)
+
+"""
+函数说明:绘制决策树
+
+Parameters:
+    myTree - 决策树(字典)
+    parentPt - 标注的内容
+    nodeTxt - 结点名
+Returns:
+    无
+Author:
+    Jack Cui
+Blog:
+    http://blog.csdn.net/c406495762
+Modify:
+    2017-07-24
+"""
+def plotTree(myTree, parentPt, nodeTxt):
+    decisionNode = dict(boxstyle="sawtooth", fc="0.8")                                        #设置结点格式
+    leafNode = dict(boxstyle="round4", fc="0.8")                                            #设置叶结点格式
+    numLeafs = getNumLeafs(myTree)                                                          #获取决策树叶结点数目，决定了树的宽度
+    depth = getTreeDepth(myTree)                                                            #获取决策树层数
+    firstStr = next(iter(myTree))                                                            #下个字典                                                 
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs))/2.0/plotTree.totalW, plotTree.yOff)    #中心位置
+    plotMidText(cntrPt, parentPt, nodeTxt)                                                    #标注有向边属性值
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)                                        #绘制结点
+    secondDict = myTree[firstStr]                                                            #下一个字典，也就是继续绘制子结点
+    plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD                                        #y偏移
+    for key in secondDict.keys():                               
+        if type(secondDict[key]).__name__=='dict':                                            #测试该结点是否为字典，如果不是字典，代表此结点为叶子结点
+            plotTree(secondDict[key],cntrPt,str(key))                                        #不是叶结点，递归调用继续绘制
+        else:                                                                                #如果是叶结点，绘制叶结点，并标注有向边属性值                                             
+            plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
+
+"""
+函数说明:创建绘制面板
+
+Parameters:
+    inTree - 决策树(字典)
+Returns:
+    无
+Author:
+    Jack Cui
+Blog:
+    http://blog.csdn.net/c406495762
+Modify:
+    2017-07-24
+"""
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')                                                    #创建fig
+    fig.clf()                                                                                #清空fig
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)                                #去掉x、y轴
+    plotTree.totalW = float(getNumLeafs(inTree))                                            #获取决策树叶结点数目
+    plotTree.totalD = float(getTreeDepth(inTree))                                            #获取决策树层数
+    plotTree.xOff = -0.5/plotTree.totalW; plotTree.yOff = 1.0;                                #x偏移
+    plotTree(inTree, (0.5,1.0), '')                                                            #绘制决策树
+    plt.show()      
     
 
 if __name__ == '__main__':
-    dataSet, features = createDataSet()
-    print(dataSet)
-    print(calcShannonEnt(dataSet))
-    print(splitDataSet(dataSet, 0, 0))
-    print(chooseBestFeatureToSplit(dataSet))
+    import pandas as pd
+    from sklearn.preprocessing import LabelEncoder
 
-    dataSet, labels = createDataSet()
-    featLabels = []
-    myTree = createTree(dataSet, labels, featLabels)
-    testVec = [0,1]                                        #测试数据
-    result = classify(myTree, featLabels, testVec)
-    if result == 'yes':
-        print('放贷')
-    if result == 'no':
-        print('不放贷')
+    import pydotplus
+    from sklearn.externals.six import StringIO
+    with open('lenses.txt', 'r') as fr:                                        #加载文件
+        lenses = [inst.strip().split('\t') for inst in fr.readlines()]        #处理文件
+    lenses_target = []                                                        #提取每组数据的类别，保存在列表里
+    for each in lenses:
+        lenses_target.append(each[-1])
 
-    myTree = {'有自己的房子': {0: {'有工作': {0: 'no', 1: 'yes'}}, 1: 'yes'}}
-    storeTree(myTree, 'classifierStorage.txt')
-
-    myTree = grabTree('classifierStorage.txt')
-    print(myTree)
+    lensesLabels = ['age', 'prescript', 'astigmatic', 'tearRate']            #特征标签       
+    lenses_list = []                                                        #保存lenses数据的临时列表
+    lenses_dict = {}                                                        #保存lenses数据的字典，用于生成pandas
+    for each_label in lensesLabels:                                            #提取信息，生成字典
+        for each in lenses:
+            lenses_list.append(each[lensesLabels.index(each_label)])
+        lenses_dict[each_label] = lenses_list
+        lenses_list = []
+    # print(lenses_dict)                                                        #打印字典信息
+    lenses_pd = pd.DataFrame(lenses_dict)                                    #生成pandas.DataFrame
+    print(lenses_pd)                                                        #打印pandas.DataFrame
+    le = LabelEncoder()                                                        #创建LabelEncoder()对象，用于序列化            
+    for col in lenses_pd.columns:                                            #为每一列序列化
+        lenses_pd[col] = le.fit_transform(lenses_pd[col])
+    print(lenses_pd)
